9f2c001973bda7a287af7ba0f74742e3dd492db4
dlrjs2360 <lhg3670@naver.com>
2022-04-08 16:21:36 +0900

    First commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+declare const module: any; // Hot Reloading Setting
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 8000;
+  await app.listen(3000);
+  console.log(`Listening on http://localhost:${port}`);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  } // Hot Reloading Setting
+}
+bootstrap();
