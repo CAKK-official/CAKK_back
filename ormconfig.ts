74293c08da9128d9eeb2513026759b153aef2c13
sunseo18 <sunseo1818@gmail.com>
2022-04-20 21:33:43 +0900

    :bug: fix: <오류를 바꾼다> .env 변수 인식 못 해서 주석추가

--- ormconfig.ts
+++ ormconfig.ts
@@ -8,6 +8,9 @@ const config: TypeOrmModuleOptions = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  // username: 'cakk',
+  // password: 'nestjshi123!',
+  // database: 'cakk',
   entities: [CakeStore],
   synchronize: false,
 };
