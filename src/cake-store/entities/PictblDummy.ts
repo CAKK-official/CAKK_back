ff1f1f9a354cca1482af7f41af2eeff2d44f8372
sunseo18 <sunseo1818@gmail.com>
2022-04-22 23:43:26 +0900

    :sparkles: feat: <새로운 기능> entity 파일 @ApiProperty 설정

--- src/cake-store/entities/PictblDummy.ts
+++ src/cake-store/entities/PictblDummy.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   Column,
   Entity,
@@ -12,15 +13,30 @@ import { StoretblDummy } from './StoretblDummy';
 @Entity('pictbl_dummy', { schema: 'cakk' })
 export class PictblDummy {
   @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
+  @ApiProperty({
+    description: '사진테이블 PK',
+    example: 1,
+  })
   id: number;
 
   @Column('int', { name: 'storeid', nullable: false })
+  @ApiProperty({
+    description: '케이크 가게 id',
+    example: 1,
+  })
   storeid: number | null;
 
   @Column('varchar', { name: 'url', length: 500 })
+  @ApiProperty({
+    description: '사진 주소',
+  })
   url: string;
 
   @Column('json', { name: 'category' })
+  @ApiProperty({
+    description: '사진 카테고리',
+    example: 'letter',
+  })
   category: object;
 
   @ManyToOne(
